ddca7b1fccf12bb231b89202dd5f2838db592c76
Klyff Harlley <klyff.harlley@gmail.com>
2016-04-21 09:34:48 -0300

    Cleaning the repo

--- target/classes/web/nuxeo.war/pdfviewer/generic/annotationeer/scripts/default_override.js
+++
@@ -1,5 +0,0 @@
-/**
- * This file is intended to override variable settings found in default.js. Please do not change the values
- * in default.js but instead set them here. That way, you will avoid the hassle of double checking merges and
- * or any new additions from getting reset.
- */
--- target/classes/web/nuxeo.war/pdfviewer/generic/annotationeer/scripts/pdf_document.js
+++
@@ -1,8 +0,0 @@
-function PDFDocument() {
-    this.documentId = 0;
-    this.totalPages = 0;
-    this.isFirst = false;
-    this.isLast = false;
-}
-
-PDFDocument.prototype = {};
